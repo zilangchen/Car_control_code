97761a62469ca94ddaade34f61a9211aacd2cac1
Mirulis <xumo2071@163.com>
2024-05-09 22:06:39 +0800

    Update motor.c
    240509 motor.c change pin

--- F407VET6_大车麦轮代码_C50B版本_(2023.05.31)/HARDWARE/motor.c
+++ F407VET6_大车麦轮代码_C50B版本_(2023.05.31)/HARDWARE/motor.c
@@ -13,21 +13,21 @@ void MiniBalance_Motor_Init(void)
   GPIO_InitTypeDef  GPIO_InitStructure;
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA|RCC_AHB1Periph_GPIOB|RCC_AHB1Periph_GPIOC|RCC_AHB1Periph_GPIOD, ENABLE);//使能GPIOB C D时钟	
 
-	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4|GPIO_Pin_5;//对应IO口
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_8;//对应IO口
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;//50MHz
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
   GPIO_Init(GPIOA, &GPIO_InitStructure);//初始化GPIO
 	
-	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12|GPIO_Pin_13 |GPIO_Pin_14;//| GPIO_Pin_15; 	
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_1|GPIO_Pin_12|GPIO_Pin_13 |GPIO_Pin_14;//| GPIO_Pin_15; 	
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; 
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
 	GPIO_Init(GPIOB, &GPIO_InitStructure); 	
 	
-	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0;//对应IO口
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0|GPIO_Pin_4|GPIO_Pin_12;//对应IO口
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;//50MHz
@@ -42,9 +42,9 @@ void MiniBalance_Motor_Init(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
   GPIO_Init(GPIOD, &GPIO_InitStructure);//初始化GPIO
 	
-	GPIO_ResetBits(GPIOA,GPIO_Pin_4|GPIO_Pin_5);
-	GPIO_ResetBits(GPIOB,GPIO_Pin_12|GPIO_Pin_13 |GPIO_Pin_14);
-	GPIO_ResetBits(GPIOC,GPIO_Pin_0);
+	GPIO_ResetBits(GPIOA,GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_8);
+	GPIO_ResetBits(GPIOB,GPIO_Pin_1|GPIO_Pin_12|GPIO_Pin_13 |GPIO_Pin_14);
+	GPIO_ResetBits(GPIOC,GPIO_Pin_0|GPIO_Pin_4|GPIO_Pin_12);
 	GPIO_ResetBits(GPIOD,GPIO_Pin_10|GPIO_Pin_12);
 
 }
